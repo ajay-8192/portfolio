6779c5cc2f4b263ea60b19c20ed1cd2122a75b57
ajay-8192 <ajaychowdary3699@gmail.com>
2025-07-22 13:29:37 +0530

    add dockerfile

--- apps/portfolio/vite.config.ts
+++ apps/portfolio/vite.config.ts
@@ -19,6 +19,7 @@ export default defineConfig(() => ({
   //  plugins: [ nxViteTsPaths() ],
   // },
   build: {
+    assetsDir: 'portfolio',
     outDir: './dist',
     emptyOutDir: true,
     reportCompressedSize: true,
